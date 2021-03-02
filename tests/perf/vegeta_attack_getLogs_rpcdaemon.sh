072dbc0314f383fbe236fc0c26e34187fe2191ca
canepat <tullio.canepa@gmail.com>
2021-03-03 00:40:49 +0100

    Add command-line parameters to workload scripts

--- tests/perf/vegeta_attack_getLogs_rpcdaemon.sh
+++ tests/perf/vegeta_attack_getLogs_rpcdaemon.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-VEGETA_BODY_FILE=/tmp/turbo_geth_stress_test/vegeta_turbo_geth_eth_getLogs.txt
-cat $VEGETA_BODY_FILE | vegeta attack -rate=100 -format=json -duration=30s -timeout=300s | vegeta report -type=text > getLogs_100qps_30s_rpcdaemon_perf.hrd
+RATE=${1:-200}
+DURATION=${2:-30}
+TIMEOUT=${3:-300}
+VEGETA_BODY_FILE=${4:-/tmp/turbo_geth_stress_test/vegeta_turbo_geth_eth_getLogs.txt}
+
+cat $VEGETA_BODY_FILE |
+    vegeta attack -keepalive -rate=${RATE} -format=json -duration=${DURATION}s -timeout=${TIMEOUT}s |
+        vegeta report -type=text > getLogs_${RATE}qps_${DURATION}s_rpcdaemon_perf.hrd
