b693c5d734547e2d8b5dcb0a3199ad9472dc4162
canepat <16927169+canepat@users.noreply.github.com>
2021-06-16 00:12:21 +0200

    Templatize ETHBACKEND gRPC unary clients

--- silkrpc/ethbackend/client.hpp
+++ silkrpc/ethbackend/client.hpp
@@ -17,7 +17,93 @@
 #ifndef SILKRPC_ETHBACKEND_CLIENT_HPP_
 #define SILKRPC_ETHBACKEND_CLIENT_HPP_
 
-#include <silkrpc/ethbackend/etherbase_client.hpp>
-#include <silkrpc/ethbackend/protocol_version_client.hpp>
+#include <functional>
+#include <memory>
+
+#include <grpcpp/grpcpp.h>
+#include <silkworm/common/magic_enum.hpp>
+
+#include <silkrpc/common/log.hpp>
+#include <silkrpc/grpc/async_completion_handler.hpp>
+#include <silkrpc/interfaces/remote/ethbackend.grpc.pb.h>
+
+namespace silkrpc::ethbackend {
+
+template<
+    typename Stub,
+    std::unique_ptr<Stub>(*NewStub)(const std::shared_ptr<::grpc::ChannelInterface>&, const ::grpc::StubOptions&),
+    typename Request,
+    typename Reply,
+    std::unique_ptr<::grpc::ClientAsyncResponseReader<Reply>>(Stub::*PrepareAsync)(::grpc::ClientContext*, const Request&, ::grpc::CompletionQueue*)
+>
+class AsyncUnaryClient final : public grpc::AsyncCompletionHandler {
+    using AsyncResponseReaderPtr = std::unique_ptr<::grpc::ClientAsyncResponseReader<Reply>>;
+
+    enum CallStatus { CALL_IDLE, CALL_STARTED, CALL_ENDED };
+
+public:
+    explicit AsyncUnaryClient(std::shared_ptr<::grpc::Channel> channel, ::grpc::CompletionQueue* queue)
+    : queue_(queue), stub_{NewStub(channel, ::grpc::StubOptions())} {
+        SILKRPC_TRACE << "AsyncUnaryClient::ctor " << this << " state: " << magic_enum::enum_name(state_) << "\n";
+    }
+
+    ~AsyncUnaryClient() {
+        SILKRPC_TRACE << "AsyncUnaryClient::dtor " << this << " state: " << magic_enum::enum_name(state_) << "\n";
+    }
+
+    void async_call(std::function<void(const ::grpc::Status&, const Reply&)> completed) {
+        SILKRPC_TRACE << "AsyncUnaryClient::async_call " << this << " state: " << magic_enum::enum_name(state_) << " start\n";
+        completed_ = completed;
+        ::grpc::ClientContext context;
+        client_ = (stub_.get()->*PrepareAsync)(&context, Request{}, queue_);
+        state_ = CALL_STARTED;
+        client_->StartCall();
+        client_->Finish(&reply_, &result_, grpc::AsyncCompletionHandler::tag(this));
+        SILKRPC_TRACE << "AsyncUnaryClient::async_call " << this << " state: " << magic_enum::enum_name(state_) << " end\n";
+    }
+
+    void completed(bool ok) override {
+        SILKRPC_TRACE << "AsyncUnaryClient::completed " << this << " state: " << magic_enum::enum_name(state_) << " ok: " << ok << " start\n";
+        if (state_ != CALL_STARTED) {
+            throw std::runtime_error("AsyncUnaryClient::completed unexpected state");
+        }
+        SILKRPC_TRACE << "AsyncUnaryClient::completed result: " << result_.ok() << "\n";
+        if (!result_.ok()) {
+            SILKRPC_ERROR << "AsyncUnaryClient::completed error_code: " << result_.error_code() << "\n";
+            SILKRPC_ERROR << "AsyncUnaryClient::completed error_message: " << result_.error_message() << "\n";
+            SILKRPC_ERROR << "AsyncUnaryClient::completed error_details: " << result_.error_details() << "\n";
+        }
+        state_ = CALL_ENDED;
+        completed_(result_, reply_);
+        SILKRPC_TRACE << "AsyncUnaryClient::completed " << this << " state: " << magic_enum::enum_name(state_) << " end\n";
+    }
+
+private:
+    ::grpc::CompletionQueue* queue_;
+    std::unique_ptr<Stub> stub_;
+    AsyncResponseReaderPtr client_;
+    Reply reply_;
+    ::grpc::Status result_;
+    CallStatus state_{CALL_IDLE};
+    std::function<void(const ::grpc::Status&, const Reply&)> completed_;
+};
+
+using EtherbaseClient = AsyncUnaryClient<
+    ::remote::ETHBACKEND::Stub,
+    ::remote::ETHBACKEND::NewStub,
+    ::remote::EtherbaseRequest,
+    ::remote::EtherbaseReply,
+    &::remote::ETHBACKEND::Stub::PrepareAsyncEtherbase
+>;
+
+using ProtocolVersionClient = AsyncUnaryClient<
+    ::remote::ETHBACKEND::Stub,
+    ::remote::ETHBACKEND::NewStub,
+    ::remote::ProtocolVersionRequest,
+    ::remote::ProtocolVersionReply,
+    &::remote::ETHBACKEND::Stub::PrepareAsyncProtocolVersion
+>;
+
+} // namespace silkrpc::ethbackend
 
 #endif // SILKRPC_ETHBACKEND_CLIENT_HPP_
