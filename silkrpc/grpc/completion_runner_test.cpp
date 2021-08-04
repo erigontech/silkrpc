f7d488f98a9b19af68ffc47bf366494d436e6bcb
canepat <16927169+canepat@users.noreply.github.com>
2021-08-04 23:28:48 +0200

    Add unit tests

--- silkrpc/grpc/completion_runner_test.cpp
+++ silkrpc/grpc/completion_runner_test.cpp
@@ -16,11 +16,176 @@
 
 #include "completion_runner.hpp"
 
+#include <mutex>
+#include <thread>
+
+#include <asio/io_context.hpp>
 #include <catch2/catch.hpp>
+#include <grpcpp/alarm.h>
+#include <grpcpp/support/time.h>
+
+#include <silkrpc/common/log.hpp>
+#include <silkrpc/grpc/async_completion_handler.hpp>
 
 namespace silkrpc {
 
 using Catch::Matchers::Message;
+
+int64_t g_fixture_slowdown_factor = 1;
+int64_t g_poller_slowdown_factor = 1;
+
+bool built_under_valgrind() {
+#ifdef RUNNING_ON_VALGRIND
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool built_under_tsan() {
+#if defined(__has_feature)
+#if __has_feature(thread_sanitizer)
+  return true;
+#else
+  return false;
+#endif
+#else
+#ifdef THREAD_SANITIZER
+  return true;
+#else
+  return false;
+#endif
+#endif
+}
+
+bool built_under_asan() {
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer)
+  return true;
+#else
+  return false;
+#endif
+#else
+#ifdef ADDRESS_SANITIZER
+  return true;
+#else
+  return false;
+#endif
+#endif
+}
+
+bool built_under_msan() {
+#if defined(__has_feature)
+#if __has_feature(memory_sanitizer)
+  return true;
+#else
+  return false;
+#endif
+#else
+#ifdef MEMORY_SANITIZER
+  return true;
+#else
+  return false;
+#endif
+#endif
+}
+
+bool built_under_ubsan() {
+#ifdef GRPC_UBSAN
+  return true;
+#else
+  return false;
+#endif
+}
+
+int64_t grpc_test_sanitizer_slowdown_factor() {
+    int64_t sanitizer_multiplier = 1;
+    if (built_under_valgrind()) {
+        sanitizer_multiplier = 20;
+    } else if (built_under_tsan()) {
+        sanitizer_multiplier = 5;
+    } else if (built_under_asan()) {
+        sanitizer_multiplier = 3;
+    } else if (built_under_msan()) {
+        sanitizer_multiplier = 4;
+    } else if (built_under_ubsan()) {
+        sanitizer_multiplier = 5;
+    }
+    return sanitizer_multiplier;
+}
+
+int64_t grpc_test_slowdown_factor() {
+    return grpc_test_sanitizer_slowdown_factor() * g_fixture_slowdown_factor * g_poller_slowdown_factor;
+}
+
+gpr_timespec grpc_timeout_milliseconds_to_deadline(int64_t time_ms) {
+    return gpr_time_add(
+        gpr_now(GPR_CLOCK_MONOTONIC),
+        gpr_time_from_micros(
+            grpc_test_slowdown_factor() * static_cast<int64_t>(1e3) * time_ms,
+            GPR_TIMESPAN));
+}
+
+TEST_CASE("start completion runner", "[silkrpc][grpc][completion_runner]") {
+    SILKRPC_LOG_VERBOSITY(LogLevel::None);
+
+    SECTION("waiting on empty completion queue") {
+        grpc::CompletionQueue queue;
+        asio::io_context io_context;
+        CompletionRunner completion_runner{queue, io_context};
+        auto completion_runner_thread = std::thread([&]() { completion_runner.run(); });
+        std::this_thread::yield();
+        completion_runner.stop();
+        CHECK_NOTHROW(completion_runner_thread.join());
+    }
+
+    SECTION("posting handler completion to I/O execution context") {
+        grpc::CompletionQueue queue;
+        asio::io_context io_context;
+        CompletionRunner completion_runner{queue, io_context};
+        auto io_context_thread = std::thread([&]() { io_context.run(); });
+        auto completion_runner_thread = std::thread([&]() { completion_runner.run(); });
+        std::this_thread::yield();
+        std::mutex m;
+        class ACH : public AsyncCompletionHandler {
+        public:
+            explicit ACH(std::mutex& m) : m_(m) {}
+            void completed(bool ok) override { m_.unlock(); };
+        private:
+            std::mutex& m_;
+        };
+        ACH handler{m};
+        AsyncCompletionHandler* handler_ptr = &handler;
+        grpc::Alarm alarm;
+        alarm.Set(&queue, grpc_timeout_milliseconds_to_deadline(10), AsyncCompletionHandler::tag(handler_ptr));
+        m.lock();
+        completion_runner.stop();
+        io_context.stop();
+        CHECK_NOTHROW(io_context_thread.join());
+        CHECK_NOTHROW(completion_runner_thread.join());
+    }
+
+    SECTION("exiting on completion queue already shutdown") {
+        grpc::CompletionQueue queue;
+        asio::io_context io_context;
+        CompletionRunner completion_runner{queue, io_context};
+        completion_runner.stop();
+        auto completion_runner_thread = std::thread([&]() { completion_runner.run(); });
+        std::this_thread::yield();
+        CHECK_NOTHROW(completion_runner_thread.join());
+    }
+
+    SECTION("stopping again after already stopped") {
+        grpc::CompletionQueue queue;
+        asio::io_context io_context;
+        CompletionRunner completion_runner{queue, io_context};
+        auto completion_runner_thread = std::thread([&]() { completion_runner.run(); });
+        std::this_thread::yield();
+        completion_runner.stop();
+        CHECK_NOTHROW(completion_runner_thread.join());
+        CHECK_NOTHROW(completion_runner.stop());
+    }
+}
 
 } // namespace silkrpc
 
