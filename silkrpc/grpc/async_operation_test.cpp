c226546aa7534cf529af19197890c3c07d654f69
canepat <16927169+canepat@users.noreply.github.com>
2021-08-05 23:23:13 +0200

    Add unit test for grpc async operation

--- silkrpc/grpc/async_operation_test.cpp
+++ silkrpc/grpc/async_operation_test.cpp
@@ -16,11 +16,27 @@
 
 #include "async_operation.hpp"
 
+#include <asio/error_code.hpp>
 #include <catch2/catch.hpp>
 
 namespace silkrpc {
 
 using Catch::Matchers::Message;
+
+TEST_CASE("call hook on async operation completion", "[silkrpc][grpc][async_operation]") {
+    class AO : public async_operation<void, asio::error_code> {
+    public:
+        AO() : async_operation<void, asio::error_code>(&AO::do_complete) {}
+        static void do_complete(void* owner, async_operation<void, asio::error_code>* base, asio::error_code error = {}) {
+            auto self = static_cast<AO*>(base);
+            self->called = true;
+        }
+        bool called{false};
+    };
+    AO op{};
+    op.complete(reinterpret_cast<void*>(1), {});
+    CHECK(op.called == true);
+}
 
 } // namespace silkrpc
 
