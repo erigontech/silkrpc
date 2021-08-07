6b66b3b70717f410a8ba05959d11e8d48b8d6efd
canepat <16927169+canepat@users.noreply.github.com>
2021-08-07 16:11:26 +0200

    Add unit tests for async operation

--- silkrpc/grpc/async_operation_test.cpp
+++ silkrpc/grpc/async_operation_test.cpp
@@ -110,4 +110,73 @@ TEST_CASE("give future result on async void operation with delayed completion", 
     CHECK_NOTHROW(future_result.get());
 }
 
+class initiate_async_op_immediate {
+public:
+    using executor_type = asio::io_context::executor_type;
+
+    explicit initiate_async_op_immediate(executor_type executor, uint32_t id) : executor_(executor), id_(id) {}
+
+    executor_type get_executor() const noexcept { return executor_; }
+
+    template<typename WaitHandler>
+    void operator()(WaitHandler&& handler) {
+        using async_op = async_reply_operation<WaitHandler, asio::io_context::executor_type, uint32_t>;
+        asio::detail::non_const_lvalue<WaitHandler> handler2(handler);
+        auto op = new async_op(handler2.value, executor_);
+        op->complete(op, {}, id_);
+    }
+private:
+    executor_type executor_;
+    uint32_t id_;
+};
+
+template<typename WaitHandler>
+auto async_op_immediate(WaitHandler&& handler, uint32_t id, asio::io_context& io_context) {
+    return asio::async_initiate<WaitHandler, void(asio::error_code, uint32_t)>(initiate_async_op_immediate{io_context.get_executor(), id}, handler);
+}
+
+TEST_CASE("give future result on async operation with immediate completion", "[silkrpc][grpc][async_operation]") {
+    asio::io_context io_context;
+    using use_future_t_lvalue_ref = typename std::add_lvalue_reference<asio::use_future_t<>>::type;
+    asio::detail::non_const_lvalue<asio::use_future_t<>> handler(const_cast<use_future_t_lvalue_ref>(asio::use_future));
+    auto future_result = async_op_immediate(asio::use_future, 18, io_context);
+    CHECK_NOTHROW(future_result.get() == 18);
+}
+
+class initiate_async_op_delayed {
+public:
+    using executor_type = asio::io_context::executor_type;
+
+    explicit initiate_async_op_delayed(executor_type executor, uint32_t id) : executor_(executor), id_(id) {}
+
+    executor_type get_executor() const noexcept { return executor_; }
+
+    template<typename WaitHandler>
+    void operator()(WaitHandler&& handler) {
+        using async_op = async_reply_operation<WaitHandler, asio::io_context::executor_type, uint32_t>;
+        asio::detail::non_const_lvalue<WaitHandler> handler2(handler);
+        auto op = new async_op(handler2.value, executor_);
+        auto result = std::async([&]() {
+            std::this_thread::yield();
+            op->complete(op, {}, id_);
+        });
+    }
+private:
+    executor_type executor_;
+    uint32_t id_;
+};
+
+template<typename WaitHandler>
+auto async_op_delayed(WaitHandler&& handler, uint32_t id, asio::io_context& io_context) {
+    return asio::async_initiate<WaitHandler, void(asio::error_code, uint32_t)>(initiate_async_op_delayed{io_context.get_executor(), id}, handler);
+}
+
+TEST_CASE("give future result on async operation with delayed completion", "[silkrpc][grpc][async_operation]") {
+    asio::io_context io_context;
+    using use_future_t_lvalue_ref = typename std::add_lvalue_reference<asio::use_future_t<>>::type;
+    asio::detail::non_const_lvalue<asio::use_future_t<>> handler(const_cast<use_future_t_lvalue_ref>(asio::use_future));
+    auto future_result = async_op_delayed(asio::use_future, 18, io_context);
+    CHECK_NOTHROW(future_result.get() == 18);
+}
+
 } // namespace silkrpc
