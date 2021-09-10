219749fc1a4d24341084ce76557e8b4b887c7062
sixtysixter <giacomo66.guarguaglini@gmail.com>
2021-09-10 21:33:04 +0200

    Added SplitCursor

--- silkrpc/ethdb/cursor.hpp
+++ silkrpc/ethdb/cursor.hpp
@@ -56,6 +56,40 @@ public:
     virtual asio::awaitable<KeyValue> seek_both_exact(const silkworm::ByteView& key, const silkworm::ByteView& value) = 0;
 };
 
+struct SplittedKeyValue {
+    silkworm::Bytes key1;
+    silkworm::Bytes key2;
+    silkworm::Bytes key3;
+    silkworm::Bytes value;
+};
+
+class SplitCursor {
+public:
+    SplitCursor(Cursor& inner_cursor,
+        const silkworm::Bytes& key,
+        uint64_t match_bits,
+        uint64_t length1,
+        uint64_t length2);
+    SplitCursor& operator=(const SplitCursor&) = delete;
+
+    asio::awaitable<SplittedKeyValue> seek();
+
+    asio::awaitable<SplittedKeyValue> next();
+
+private:
+    Cursor& inner_cursor_;
+    silkworm::Bytes key_;
+    silkworm::Bytes first_bytes_;
+    uint8_t last_bits_;
+    uint64_t length1_;
+    uint64_t length2_;
+    uint64_t match_bytes_;
+    uint8_t mask_;
+
+    bool match_key(const silkworm::ByteView& key);
+    SplittedKeyValue split_key_value(const KeyValue& kv);
+};
+
 } // namespace silkrpc::ethdb
 
 #endif  // SILKRPC_ETHDB_CURSOR_HPP_
