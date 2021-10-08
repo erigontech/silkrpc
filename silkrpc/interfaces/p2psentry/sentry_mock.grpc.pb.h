82e6888bc71569e975d80f61c5ce864e9adc4407
Paolob69 <bruzzopa69@gmail.com>
2021-10-08 19:56:01 +0200

    updates for new grpc interface

--- silkrpc/interfaces/p2psentry/sentry_mock.grpc.pb.h
+++ silkrpc/interfaces/p2psentry/sentry_mock.grpc.pb.h
@@ -12,12 +12,18 @@ namespace sentry {
 
 class MockSentryStub : public Sentry::StubInterface {
  public:
+  MOCK_METHOD3(SetStatus, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::StatusData& request, ::sentry::SetStatusReply* response));
+  MOCK_METHOD3(AsyncSetStatusRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SetStatusReply>*(::grpc::ClientContext* context, const ::sentry::StatusData& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD3(PrepareAsyncSetStatusRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SetStatusReply>*(::grpc::ClientContext* context, const ::sentry::StatusData& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(PenalizePeer, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::PenalizePeerRequest& request, ::google::protobuf::Empty* response));
   MOCK_METHOD3(AsyncPenalizePeerRaw, ::grpc::ClientAsyncResponseReaderInterface< ::google::protobuf::Empty>*(::grpc::ClientContext* context, const ::sentry::PenalizePeerRequest& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(PrepareAsyncPenalizePeerRaw, ::grpc::ClientAsyncResponseReaderInterface< ::google::protobuf::Empty>*(::grpc::ClientContext* context, const ::sentry::PenalizePeerRequest& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(PeerMinBlock, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::PeerMinBlockRequest& request, ::google::protobuf::Empty* response));
   MOCK_METHOD3(AsyncPeerMinBlockRaw, ::grpc::ClientAsyncResponseReaderInterface< ::google::protobuf::Empty>*(::grpc::ClientContext* context, const ::sentry::PeerMinBlockRequest& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(PrepareAsyncPeerMinBlockRaw, ::grpc::ClientAsyncResponseReaderInterface< ::google::protobuf::Empty>*(::grpc::ClientContext* context, const ::sentry::PeerMinBlockRequest& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD3(HandShake, ::grpc::Status(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::sentry::HandShakeReply* response));
+  MOCK_METHOD3(AsyncHandShakeRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::HandShakeReply>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD3(PrepareAsyncHandShakeRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::HandShakeReply>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(SendMessageByMinBlock, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::SendMessageByMinBlockRequest& request, ::sentry::SentPeers* response));
   MOCK_METHOD3(AsyncSendMessageByMinBlockRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SentPeers>*(::grpc::ClientContext* context, const ::sentry::SendMessageByMinBlockRequest& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(PrepareAsyncSendMessageByMinBlockRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SentPeers>*(::grpc::ClientContext* context, const ::sentry::SendMessageByMinBlockRequest& request, ::grpc::CompletionQueue* cq));
@@ -30,18 +36,15 @@ class MockSentryStub : public Sentry::StubInterface {
   MOCK_METHOD3(SendMessageToAll, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::OutboundMessageData& request, ::sentry::SentPeers* response));
   MOCK_METHOD3(AsyncSendMessageToAllRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SentPeers>*(::grpc::ClientContext* context, const ::sentry::OutboundMessageData& request, ::grpc::CompletionQueue* cq));
   MOCK_METHOD3(PrepareAsyncSendMessageToAllRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SentPeers>*(::grpc::ClientContext* context, const ::sentry::OutboundMessageData& request, ::grpc::CompletionQueue* cq));
-  MOCK_METHOD3(SetStatus, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::StatusData& request, ::sentry::SetStatusReply* response));
-  MOCK_METHOD3(AsyncSetStatusRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SetStatusReply>*(::grpc::ClientContext* context, const ::sentry::StatusData& request, ::grpc::CompletionQueue* cq));
-  MOCK_METHOD3(PrepareAsyncSetStatusRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::SetStatusReply>*(::grpc::ClientContext* context, const ::sentry::StatusData& request, ::grpc::CompletionQueue* cq));
-  MOCK_METHOD2(ReceiveMessagesRaw, ::grpc::ClientReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request));
-  MOCK_METHOD4(AsyncReceiveMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq, void* tag));
-  MOCK_METHOD3(PrepareAsyncReceiveMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq));
-  MOCK_METHOD2(ReceiveUploadMessagesRaw, ::grpc::ClientReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request));
-  MOCK_METHOD4(AsyncReceiveUploadMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq, void* tag));
-  MOCK_METHOD3(PrepareAsyncReceiveUploadMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq));
-  MOCK_METHOD2(ReceiveTxMessagesRaw, ::grpc::ClientReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request));
-  MOCK_METHOD4(AsyncReceiveTxMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq, void* tag));
-  MOCK_METHOD3(PrepareAsyncReceiveTxMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::google::protobuf::Empty& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD2(MessagesRaw, ::grpc::ClientReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::sentry::MessagesRequest& request));
+  MOCK_METHOD4(AsyncMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::sentry::MessagesRequest& request, ::grpc::CompletionQueue* cq, void* tag));
+  MOCK_METHOD3(PrepareAsyncMessagesRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::InboundMessage>*(::grpc::ClientContext* context, const ::sentry::MessagesRequest& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD3(PeerCount, ::grpc::Status(::grpc::ClientContext* context, const ::sentry::PeerCountRequest& request, ::sentry::PeerCountReply* response));
+  MOCK_METHOD3(AsyncPeerCountRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::PeerCountReply>*(::grpc::ClientContext* context, const ::sentry::PeerCountRequest& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD3(PrepareAsyncPeerCountRaw, ::grpc::ClientAsyncResponseReaderInterface< ::sentry::PeerCountReply>*(::grpc::ClientContext* context, const ::sentry::PeerCountRequest& request, ::grpc::CompletionQueue* cq));
+  MOCK_METHOD2(PeersRaw, ::grpc::ClientReaderInterface< ::sentry::PeersReply>*(::grpc::ClientContext* context, const ::sentry::PeersRequest& request));
+  MOCK_METHOD4(AsyncPeersRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::PeersReply>*(::grpc::ClientContext* context, const ::sentry::PeersRequest& request, ::grpc::CompletionQueue* cq, void* tag));
+  MOCK_METHOD3(PrepareAsyncPeersRaw, ::grpc::ClientAsyncReaderInterface< ::sentry::PeersReply>*(::grpc::ClientContext* context, const ::sentry::PeersRequest& request, ::grpc::CompletionQueue* cq));
 };
 
 } // namespace sentry
