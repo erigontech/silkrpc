8b6afc0bc0e552487eab6dcc3418410f41bb6855
Paolob69 <bruzzopa69@gmail.com>
2021-10-14 22:00:55 +0200

    asunc_start tested

--- silkrpc/ethdb/kv/awaitables_test.cpp
+++ silkrpc/ethdb/kv/awaitables_test.cpp
@@ -33,93 +33,122 @@ namespace silkrpc::ethdb::kv {
 
 using Catch::Matchers::Message;
 
-TEST_CASE("awaitable") {
-    SECTION("async_start success ") {
-       class StreamingClientTest : public StreamingClient {
-          void start_call(std::function<void(const grpc::Status&)> start_completed) override { printf("StreamingClientTest: start_call\n"); start_completed(::grpc::Status::OK);}
-          void end_call(std::function<void(const grpc::Status&)> end_completed) override {}
-          void read_start(std::function<void(const grpc::Status&, ::remote::Pair)> read_completed) override {printf("StreamingClientTest: read_call\n"); pair.set_txid(4); read_completed(::grpc::Status::OK, pair);}
-          void write_start(const ::remote::Cursor& cursor, std::function<void(const grpc::Status&)> write_completed) override {}
-          void completed(bool ok) override { printf("StreamingClientTest: completed \n");}
-
-          private:
-            ::remote::Pair pair;
-      };
-
-      class TestAwaitable {
+TEST_CASE("async_start") {
+      class AwaitableWrap { 
        public:
-         TestAwaitable(asio::io_context& context, StreamingClientTest client) : kv_awaitable_{context, client} {}
-         virtual ~TestAwaitable() {}
-         asio::awaitable<int64_t> open() {
-            printf("StreamingClientTest: open\n");
+         AwaitableWrap(asio::io_context& context, StreamingClient& client) : kv_awaitable_{context, client} {} // tipo interfaccia
+         virtual ~AwaitableWrap() {}
+         asio::awaitable<int64_t> async_start() {
             int64_t tx_id = co_await kv_awaitable_.async_start(asio::use_awaitable);
             co_return tx_id;
          }
-
-         StreamingClientTest client_;
+         asio::awaitable<int64_t> async_open_cursor(const std::string& table_name) {
+            uint32_t cursor_id = co_await kv_awaitable_.async_open_cursor(table_name, asio::use_awaitable);
+            co_return cursor_id;
+         }
          KvAsioAwaitable<asio::io_context::executor_type> kv_awaitable_;
+      };
+
+    SECTION("success with sync call") {
+       class MockStreamingClient : public StreamingClient { // asincrona std::async MockStreamingClient
+          void start_call(std::function<void(const grpc::Status&)> start_completed) override {  start_completed(::grpc::Status::OK);}
+          void end_call(std::function<void(const grpc::Status&)> end_completed) override {}
+          void read_start(std::function<void(const grpc::Status&, ::remote::Pair)> read_completed) override { ::remote::Pair pair; pair.set_txid(4); read_completed(::grpc::Status::OK, pair);}
+          void write_start(const ::remote::Cursor& cursor, std::function<void(const grpc::Status&)> write_completed) override {}
+          void completed(bool ok) override { }
       };
 
       ContextPool cp{1, []() { return grpc::CreateChannel("localhost", grpc::InsecureChannelCredentials()); }};
       auto context_pool_thread = std::thread([&]() { cp.run(); });
-
       int64_t txid;
       try {
-        StreamingClientTest sct;
-        TestAwaitable test{*(cp.get_context().io_context), sct };
-        auto result{asio::co_spawn(cp.get_io_context(), test.open(), asio::use_future)};
+        MockStreamingClient sct;
+        AwaitableWrap test{*(cp.get_context().io_context), sct }; 
+        auto result{asio::co_spawn(cp.get_io_context(), test.async_start(), asio::use_future)};
         txid = result.get();
        } catch (...) {
            CHECK(false);
        }
-
        CHECK(txid == 4);
        cp.stop();
        context_pool_thread.join();
     }
 
-    SECTION("async_start fails ") {
-       class StreamingClientTest : public StreamingClient {
-          void start_call(std::function<void(const grpc::Status&)> start_completed) override { printf("StreamingClientTest: start_call\n"); start_completed(::grpc::Status::CANCELLED); }
+    SECTION("success with async call") {
+       class MockStreamingClient : public StreamingClient { 
+          void start_call(std::function<void(const grpc::Status&)> start_completed) override { 
+            auto result = std::async([&]() {
+               std::this_thread::yield();
+               start_completed(::grpc::Status::OK);
+            });
+          }
           void end_call(std::function<void(const grpc::Status&)> end_completed) override {}
-          void read_start(std::function<void(const grpc::Status&, ::remote::Pair)> read_completed) override {printf("StreamingClientTest: read_call\n"); pair.set_txid(4); read_completed(::grpc::Status::OK, pair);}
+          void read_start(std::function<void(const grpc::Status&, ::remote::Pair)> read_completed) override { ::remote::Pair pair; pair.set_txid(4); read_completed(::grpc::Status::OK, pair);}
           void write_start(const ::remote::Cursor& cursor, std::function<void(const grpc::Status&)> write_completed) override {}
-          void completed(bool ok) override { printf("StreamingClientTest: completed \n"); }
-
-          private:
-            ::remote::Pair pair;
-      };
-
-      class TestAwaitable {
-       public:
-         TestAwaitable(asio::io_context& context, StreamingClientTest client) : kv_awaitable_{context, client} {}
-         virtual ~TestAwaitable() {}
-         asio::awaitable<int64_t> open() {
-            printf("StreamingClientTest: open\n");
-            int64_t tx_id = co_await kv_awaitable_.async_start(asio::use_awaitable);
-            co_return tx_id;
-         }
-
-         StreamingClientTest client_;
-         KvAsioAwaitable<asio::io_context::executor_type> kv_awaitable_;
+          void completed(bool ok) override { }
       };
 
       ContextPool cp{1, []() { return grpc::CreateChannel("localhost", grpc::InsecureChannelCredentials()); }};
       auto context_pool_thread = std::thread([&]() { cp.run(); });
-
       int64_t txid;
       try {
-        StreamingClientTest sct;
-        TestAwaitable test{*(cp.get_context().io_context), sct };
-        auto result{asio::co_spawn(cp.get_io_context(), test.open(), asio::use_future)};
+        MockStreamingClient sct;
+        AwaitableWrap test{*(cp.get_context().io_context), sct }; 
+        auto result{asio::co_spawn(cp.get_io_context(), test.async_start(), asio::use_future)};
         txid = result.get();
        } catch (...) {
-           CHECK(true);
+           CHECK(false);
        }
-
+       CHECK(txid == 4);
        cp.stop();
        context_pool_thread.join();
     }
+
+    SECTION("start_call fails ") {
+       class MockStreamingClient : public StreamingClient {
+          void start_call(std::function<void(const grpc::Status&)> start_completed) override { start_completed(::grpc::Status::CANCELLED); }
+          void end_call(std::function<void(const grpc::Status&)> end_completed) override {}
+          void read_start(std::function<void(const grpc::Status&, ::remote::Pair)> read_completed) override {}
+          void write_start(const ::remote::Cursor& cursor, std::function<void(const grpc::Status&)> write_completed) override {}
+          void completed(bool ok) override {}
+      };
+
+      ContextPool cp{1, []() { return grpc::CreateChannel("localhost", grpc::InsecureChannelCredentials()); }};
+      auto context_pool_thread = std::thread([&]() { cp.run(); });
+      try {
+        MockStreamingClient sct;
+        AwaitableWrap test{*(cp.get_context().io_context), sct }; 
+        auto result{asio::co_spawn(cp.get_io_context(), test.async_start(), asio::use_future)};
+        result.get();
+       } catch (const std::system_error& e) { 
+             CHECK(e.code().value() == 1);
+       }
+       cp.stop();
+       context_pool_thread.join();
+    }
+
+    SECTION("read_call fails ") {
+       class MockStreamingClient : public StreamingClient {
+          void start_call(std::function<void(const grpc::Status&)> start_completed) override { start_completed(::grpc::Status::OK);}
+          void end_call(std::function<void(const grpc::Status&)> end_completed) override {}
+          void read_start(std::function<void(const grpc::Status&, ::remote::Pair)> read_completed) override { ::remote::Pair pair; pair.set_txid(4); read_completed(::grpc::Status::CANCELLED, pair);}
+          void write_start(const ::remote::Cursor& cursor, std::function<void(const grpc::Status&)> write_completed) override {}
+          void completed(bool ok) override {}
+      };
+
+      ContextPool cp{1, []() { return grpc::CreateChannel("localhost", grpc::InsecureChannelCredentials()); }};
+      auto context_pool_thread = std::thread([&]() { cp.run(); });
+      try {
+        MockStreamingClient sct;
+        AwaitableWrap test{*(cp.get_context().io_context), sct }; 
+        auto result{asio::co_spawn(cp.get_io_context(), test.async_start(), asio::use_future)};
+        result.get();
+       } catch (const std::system_error& e) { 
+             CHECK(e.code().value() == 1);
+       }
+       cp.stop();
+       context_pool_thread.join();
+   }
 }
 
 } // namespace silkrpc::ethdb::kv
