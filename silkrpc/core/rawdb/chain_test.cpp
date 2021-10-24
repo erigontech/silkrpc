35147a1f30488e66b99eea1106c3802a4f672a5a
canepat <16927169+canepat@users.noreply.github.com>
2021-10-24 18:03:41 +0200

    Add unit tests for chain Fix API doc Remove unnecessary test unit

--- silkrpc/core/rawdb/chain_test.cpp
+++ silkrpc/core/rawdb/chain_test.cpp
@@ -26,6 +26,7 @@
 #include <nlohmann/json.hpp>
 #include <silkworm/common/util.hpp>
 
+#include <silkrpc/core/blocks.hpp>
 #include <silkrpc/ethdb/tables.hpp>
 
 namespace silkrpc::core::rawdb {
@@ -51,11 +52,11 @@ static silkworm::Bytes kHeader{*silkworm::from_hex("f9025ca0209f062567c161c5f71b
     "00000000000000000000000000000000000000000000880000000000000000")};
 static silkworm::Bytes kBody{*silkworm::from_hex("c68369e45a03c0")};
 
-class DummyDatabaseReader : public DatabaseReader {
+class MockDatabaseReader : public DatabaseReader {
 public:
-    DummyDatabaseReader() = default;
-    DummyDatabaseReader(const DummyDatabaseReader&) = delete;
-    DummyDatabaseReader& operator=(const DummyDatabaseReader&) = delete;
+    MockDatabaseReader() = default;
+    MockDatabaseReader(const MockDatabaseReader&) = delete;
+    MockDatabaseReader& operator=(const MockDatabaseReader&) = delete;
 
     asio::awaitable<KeyValue> get(const std::string& table, const silkworm::ByteView& key) const override {
         silkworm::Bytes bk{key};
@@ -88,31 +89,50 @@ public:
     }
 };
 
-TEST_CASE("read_block") {
+void check_expected_block_with_hash(const silkworm::BlockWithHash& bwh) {
+    CHECK(bwh.block.header.parent_hash == silkworm::to_bytes32(*silkworm::from_hex("209f062567c161c5f71b3f57a7de277b0e95c3455050b152d785ad7524ef8ee7")));
+    CHECK(bwh.block.header.ommers_hash == silkworm::to_bytes32(*silkworm::from_hex("1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347")));
+    CHECK(bwh.block.header.beneficiary == silkworm::to_address(*silkworm::from_hex("0000000000000000000000000000000000000000")));
+    CHECK(bwh.block.header.state_root == silkworm::to_bytes32(*silkworm::from_hex("e7536c5b61ed0e0ab7f3ce7f085806d40f716689c0c086676757de401b595658")));
+    CHECK(bwh.block.header.transactions_root == silkworm::to_bytes32(*silkworm::from_hex("40be247314d834a319556d1dcf458e8707cc1aa4a416b6118474ce0c96fccb1a")));
+    CHECK(bwh.block.header.receipts_root == silkworm::to_bytes32(*silkworm::from_hex("7862fe11d10a9b237ffe9cb660f31e4bc4be66836c9bfc17310d47c60d75671f")));
+    CHECK(bwh.block.header.number == 4000000);
+    CHECK(bwh.block.header.gas_limit == 8000000);
+    CHECK(bwh.block.header.gas_used == 1996875);
+    CHECK(bwh.block.header.timestamp == 1609072811);
+    CHECK(bwh.block.header.extra_data == *silkworm::from_hex("d88301091a846765746888676f312e31352e36856c696e757800000000000000be009d0049d6f0ee8ca6764a1d3e"
+        "b519bd4d046e167ddcab467d5db31d063f2d58f266fa86c4502aa169d17762090e92b821843de69b41adbb5d86f5d114ba7f01"));
+    CHECK(bwh.block.header.mix_hash == silkworm::to_bytes32(*silkworm::from_hex("0000000000000000000000000000000000000000000000000000000000000000")));
+    CHECK(bwh.hash == silkworm::to_bytes32(*silkworm::from_hex("439816753229fc0736bf86a5048de4bc9fcdede8c91dadf88c828c76b2281dff")));
+}
+
+TEST_CASE("read_block_by_number_or_hash") {
     asio::thread_pool pool{1};
 
-    DummyDatabaseReader reader;
+    MockDatabaseReader db_reader;
 
     SECTION("read_block_by_number_or_hash by number") {
         BlockNumberOrHash bnoh{4000000};
 
-        auto result = asio::co_spawn(pool, read_block_by_number_or_hash(reader, bnoh), asio::use_future);
-        const silkworm::BlockWithHash &bwh = result.get();
+        auto result = asio::co_spawn(pool, read_block_by_number_or_hash(db_reader, bnoh), asio::use_future);
+        const silkworm::BlockWithHash bwh = result.get();
+        check_expected_block_with_hash(bwh);
+    }
 
-        CHECK(bwh.block.header.parent_hash == silkworm::to_bytes32(*silkworm::from_hex("209f062567c161c5f71b3f57a7de277b0e95c3455050b152d785ad7524ef8ee7")));
-        CHECK(bwh.block.header.ommers_hash == silkworm::to_bytes32(*silkworm::from_hex("1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347")));
-        CHECK(bwh.block.header.beneficiary == silkworm::to_address(*silkworm::from_hex("0000000000000000000000000000000000000000")));
-        CHECK(bwh.block.header.state_root == silkworm::to_bytes32(*silkworm::from_hex("e7536c5b61ed0e0ab7f3ce7f085806d40f716689c0c086676757de401b595658")));
-        CHECK(bwh.block.header.transactions_root == silkworm::to_bytes32(*silkworm::from_hex("40be247314d834a319556d1dcf458e8707cc1aa4a416b6118474ce0c96fccb1a")));
-        CHECK(bwh.block.header.receipts_root == silkworm::to_bytes32(*silkworm::from_hex("7862fe11d10a9b237ffe9cb660f31e4bc4be66836c9bfc17310d47c60d75671f")));
-        CHECK(bwh.block.header.number == 4000000);
-        CHECK(bwh.block.header.gas_limit == 8000000);
-        CHECK(bwh.block.header.gas_used == 1996875);
-        CHECK(bwh.block.header.timestamp == 1609072811);
-        CHECK(bwh.block.header.extra_data == *silkworm::from_hex("d88301091a846765746888676f312e31352e36856c696e757800000000000000be009d0049d6f0ee8ca6764a1d3e"
-            "b519bd4d046e167ddcab467d5db31d063f2d58f266fa86c4502aa169d17762090e92b821843de69b41adbb5d86f5d114ba7f01"));
-        CHECK(bwh.block.header.mix_hash == silkworm::to_bytes32(*silkworm::from_hex("0000000000000000000000000000000000000000000000000000000000000000")));
-        CHECK(bwh.hash == silkworm::to_bytes32(*silkworm::from_hex("439816753229fc0736bf86a5048de4bc9fcdede8c91dadf88c828c76b2281dff")));
+    SECTION("read_block_by_number_or_hash by hash") {
+        BlockNumberOrHash bnoh{"0x439816753229fc0736bf86a5048de4bc9fcdede8c91dadf88c828c76b2281dff"};
+
+        auto result = asio::co_spawn(pool, read_block_by_number_or_hash(db_reader, bnoh), asio::use_future);
+        const silkworm::BlockWithHash bwh = result.get();
+        check_expected_block_with_hash(bwh);
+    }
+
+    SECTION("read_block_by_number_or_hash by tag") {
+        BlockNumberOrHash bnoh{kEarliestBlockId};
+
+        auto result = asio::co_spawn(pool, read_block_by_number_or_hash(db_reader, bnoh), asio::use_future);
+        const silkworm::BlockWithHash bwh = result.get();
+        check_expected_block_with_hash(bwh);
     }
 }
 
