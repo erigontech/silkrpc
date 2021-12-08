3f0454e227b6b8ea68e3dac927648465918938a3
Paolob69 <bruzzopa69@gmail.com>
2021-12-08 12:19:08 +0100

    changes after lint

--- silkrpc/context_pool.cpp
+++ silkrpc/context_pool.cpp
@@ -42,7 +42,7 @@ ContextPool::ContextPool(std::size_t pool_size, ChannelFactory create_channel) :
     }
     SILKRPC_INFO << "ContextPool::ContextPool creating pool with size: " << pool_size << "\n";
 
-    auto block_cache = std::make_shared<silkrpc::BlockCache>(1024, true); 
+    auto block_cache = std::make_shared<silkrpc::BlockCache>(1024, true);
 
     // Create all the io_contexts and give them work to do so that their event loop will not exit until they are explicitly stopped.
     for (std::size_t i{0}; i < pool_size; ++i) {
@@ -53,7 +53,7 @@ ContextPool::ContextPool(std::size_t pool_size, ChannelFactory create_channel) :
         auto database = std::make_unique<ethdb::kv::RemoteDatabase<>>(*io_context, grpc_channel, grpc_queue.get()); // TODO(canepat): move elsewhere
         auto backend = std::make_unique<ethbackend::BackEnd>(*io_context, grpc_channel, grpc_queue.get()); // TODO(canepat): move elsewhere
         auto tx_pool = std::make_unique<txpool::TransactionPool>(*io_context, grpc_channel, grpc_queue.get()); // TODO(canepat): move elsewhere
-        //auto block_cache = std::make_unique<silkrpc::BlockCache>(1024, false); 
+        //auto block_cache = std::make_unique<silkrpc::BlockCache>(1024, false);
         contexts_.push_back({io_context, std::move(grpc_queue), std::move(grpc_runner), std::move(database), std::move(backend), std::move(tx_pool), block_cache});
         //contexts_.push_back({io_context, std::move(grpc_queue), std::move(grpc_runner), std::move(database), std::move(backend), std::move(tx_pool), std::move(block_cache)});
         SILKRPC_DEBUG << "ContextPool::ContextPool context[" << i << "] " << contexts_[i] << "\n";
