33976fb897789e94cc60b0970bc5393909984415
Paolob69 <bruzzopa69@gmail.com>
2021-12-22 20:34:03 +0100

    mod param of method of cached_chain

--- silkrpc/core/cached_chain.cpp
+++ silkrpc/core/cached_chain.cpp
@@ -20,24 +20,24 @@
 
 namespace silkrpc::core  {
 
-asio::awaitable<silkworm::BlockWithHash> read_block_by_number(const std::shared_ptr<BlockCache> cache, const rawdb::DatabaseReader& reader, uint64_t block_number) {
+asio::awaitable<silkworm::BlockWithHash> read_block_by_number(BlockCache& cache, const rawdb::DatabaseReader& reader, uint64_t block_number) {
    const auto block_hash = co_await rawdb::read_canonical_block_hash(reader, block_number);
-   auto option_block = cache->get(block_hash);
+   auto option_block = cache.get(block_hash);
    if (option_block) {
       co_return *option_block;
    }
    auto block_with_hash = co_await rawdb::read_block(reader, block_hash, block_number);
-   cache->insert(block_hash, block_with_hash);
+   cache.insert(block_hash, block_with_hash);
    co_return block_with_hash;
 }
 
-asio::awaitable<silkworm::BlockWithHash> read_block_by_hash(const std::shared_ptr<BlockCache> cache, const rawdb::DatabaseReader& reader, const evmc::bytes32& block_hash) {
-   auto option_block = cache->get(block_hash);
+asio::awaitable<silkworm::BlockWithHash> read_block_by_hash(BlockCache& cache, const rawdb::DatabaseReader& reader, const evmc::bytes32& block_hash) {
+   auto option_block = cache.get(block_hash);
    if (option_block) {
       co_return *option_block;
    }
    auto block_with_hash = co_await rawdb::read_block_by_hash(reader, block_hash);
-   cache->insert(block_hash, block_with_hash);
+   cache.insert(block_hash, block_with_hash);
    co_return block_with_hash;
 }
 
