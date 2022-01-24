4dcc6c62e84ae87cc9d86d85a7d00c5c29f23858
canepat <16927169+canepat@users.noreply.github.com>
2022-01-24 16:47:59 +0100

    Allow GCC major version greater than 10

--- rebuild_gcc_debug.sh
+++ rebuild_gcc_debug.sh
@@ -4,6 +4,6 @@ PROJECT_DIR=$PWD
 rm -fr build_gcc_debug
 mkdir build_gcc_debug
 cd build_gcc_debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc-10 -DCMAKE_CXX_COMPILER=g++-10 ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
 cd $PROJECT_DIR
 ./build_gcc_debug.sh
