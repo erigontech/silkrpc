754bd1b5548f01fc2a61ab3039d6caa0c39a3f81
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2022-02-12 23:18:04 +0100

    added engine_newPayloadV1

--- silkrpc/commands/engine_api_test.cpp
+++ silkrpc/commands/engine_api_test.cpp
@@ -29,6 +29,7 @@
 namespace silkrpc::commands {
 
 using Catch::Matchers::Message;
+using evmc::literals::operator""_bytes32;
 
 namespace {
 class BackEndMock : public ethbackend::BackEnd {
@@ -49,6 +50,7 @@ public:
     explicit EngineRpcApiTest(std::unique_ptr<ethbackend::BackEnd>& backend): EngineRpcApi(backend) {}
 
     using EngineRpcApi::handle_engine_get_payload_v1;
+    using EngineRpcApi::handle_engine_new_payload_v1;
 };
 
 using testing::InvokeWithoutArgs;
@@ -124,6 +126,109 @@ TEST_CASE("handle_engine_get_payload_v1 fails with invalid amount of params", "[
         "error":{
             "code":100,
             "message":"invalid engine_getPayloadV1 params: []"
+        },
+        "id":1,
+        "jsonrpc":"2.0" 
+    })"_json);
+
+    cp.stop();
+    context_pool_thread.join();
+}
+
+TEST_CASE("handle_engine_new_payload_v1 succeeds if request is expected payload status", "[silkrpc][engine_api]") {
+    SILKRPC_LOG_VERBOSITY(LogLevel::None);
+
+    BackEndMock backend;
+    EXPECT_CALL(backend, engine_new_payload_v1(testing::_)).WillOnce(InvokeWithoutArgs(
+        []() -> asio::awaitable<PayloadStatus> {
+            co_return PayloadStatus{
+                .status = "INVALID",
+                .latest_valid_hash = 0x0000000000000000000000000000000000000000000000000000000000000040_bytes32,
+                .validation_error = "some error"
+            };
+        }
+    ));
+
+    std::unique_ptr<ethbackend::BackEnd> backend_ptr(&backend);
+
+    nlohmann::json reply;
+    nlohmann::json request = R"({
+        "jsonrpc":"2.0",
+        "id":1,
+        "method":"engine_newPayloadV1",
+        "params":[{
+            "parentHash":"0x3b8fb240d288781d4aac94d3fd16809ee413bc99294a085798a589dae51ddd4a",
+            "suggestedFeeRecipient":"0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
+            "stateRoot":"0xca3149fa9e37db08d1cd49c9061db1002ef1cd58db2210f2115c8c989b2bdf45",
+            "receiptsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+            "logsBloom":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            "random":"0x0000000000000000000000000000000000000000000000000000000000000001",
+            "blockNumber":"0x1",
+            "gasLimit":"0x1c9c380",
+            "gasUsed":"0x0",
+            "timestamp":"0x5",
+            "extraData":"0x",
+            "baseFeePerGas":"0x7",
+            "blockHash":"0x3559e851470f6e7bbed1db474980683e8c315bfce99b2a6ef47c057c04de7858",
+            "transactions":["0xf92ebdeab45d368f6354e8c5a8ac586c"]
+        }]
+    })"_json;
+    // Initialize contex pool
+    ContextPool cp{1, []() { return grpc::CreateChannel("localhost", grpc::InsecureChannelCredentials()); }};
+    auto context_pool_thread = std::thread([&]() { cp.run(); });
+    // Initialise components
+    EngineRpcApiTest rpc(backend_ptr);
+
+    // spawn routine
+    auto result{asio::co_spawn(cp.get_io_context(), [&rpc, &reply, &request]() {
+        return rpc.handle_engine_new_payload_v1(
+            request,
+            reply
+        );
+    }, asio::use_future)};
+    result.get();
+
+    CHECK(reply == PayloadStatus{
+            .status = "INVALID",
+            .latest_valid_hash = 0x0000000000000000000000000000000000000000000000000000000000000040_bytes32,
+            .validation_error = "some error"
+        });
+
+    cp.stop();
+    context_pool_thread.join();
+    backend_ptr.release();
+}
+
+TEST_CASE("handle_engine_new_payload_v1 fails with invalid amount of params", "[silkrpc][engine_api]") {
+    SILKRPC_LOG_VERBOSITY(LogLevel::None);
+
+    nlohmann::json reply;
+    nlohmann::json request = R"({
+        "jsonrpc":"2.0",
+        "id":1,
+        "method":"engine_newPayloadV1",
+        "params":[]
+    })"_json;
+    // Initialize contex pool
+    ContextPool cp{1, []() { return grpc::CreateChannel("localhost", grpc::InsecureChannelCredentials()); }};
+    auto context_pool_thread = std::thread([&]() { cp.run(); });
+    // Initialise components
+    std::unique_ptr<ethbackend::BackEnd> backend_ptr(new BackEndMock);
+    EngineRpcApiTest rpc(backend_ptr);
+
+    // spawn routine
+    auto result{asio::co_spawn(cp.get_io_context(), [&rpc, &reply, &request]() {
+        return rpc.handle_engine_new_payload_v1(
+            request,
+            reply
+        );
+    }, asio::use_future)};
+    result.get();
+
+    CHECK(reply ==  R"({
+        "error":{
+            "code":100,
+            "message":"invalid engine_newPayloadV1 params: []"
         },
         "id":1,
         "jsonrpc":"2.0" 
--- silkrpc/commands/rpc_api_table.cpp
+++ silkrpc/commands/rpc_api_table.cpp
@@ -160,6 +160,7 @@ void RpcApiTable::add_web3_handlers() {
 
 void RpcApiTable::add_engine_handlers() {
     handlers_[http::method::k_engine_getPayloadV1] = &commands::RpcApi::handle_engine_get_payload_v1;
+    handlers_[http::method::k_engine_newPayloadV1] = &commands::RpcApi::handle_engine_new_payload_v1;
 }
 
 } // namespace silkrpc::commands
