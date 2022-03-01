d87c222cefb9eafa4789dc84939f79ec323b54dd
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-03-01 23:06:44 +0000

    to and from json in hpp

--- silkrpc/json/types.hpp
+++ silkrpc/json/types.hpp
@@ -83,6 +83,9 @@ void from_json(const nlohmann::json& json, Filter& filter);
 void to_json(nlohmann::json& json, const ExecutionPayload& execution_payload);
 void from_json(const nlohmann::json& json, ExecutionPayload& execution_payload);
 
+void to_json(nlohmann::json& json, const TransitionConfiguration& transition_configuration);
+void from_json(const nlohmann::json&, TransitionConfiguration transition_configuration);
+
 void to_json(nlohmann::json& json, const PayloadStatus& payload_status);
 
 void to_json(nlohmann::json& json, const Forks& forks);
