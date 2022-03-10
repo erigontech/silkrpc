4fc92f7b65160a854829ad6a7409f5b85191151a
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-03-10 12:30:33 +0000

    important comment

--- silkrpc/commands/engine_api.hpp
+++ silkrpc/commands/engine_api.hpp
@@ -33,6 +33,7 @@ namespace silkrpc::http { class RequestHandler; }
 
 namespace silkrpc::commands {
 
+// engine rpc api
 class EngineRpcApi {
 public:
     explicit EngineRpcApi(std::unique_ptr<ethbackend::BackEnd>& backend): backend_(backend) {}
