effd586eceeff8068b42b1d95984db078acffc00
Umut Önder <93402298+umutondersu@users.noreply.github.com>
2022-03-12 18:00:46 +0300

    Changed RemoteBuffer to Remote state in evm_executor.hpp

--- silkrpc/core/evm_executor.hpp
+++ silkrpc/core/evm_executor.hpp
@@ -65,7 +65,7 @@ private:
     const core::rawdb::DatabaseReader& db_reader_;
     const silkworm::ChainConfig& config_;
     asio::thread_pool& workers_;
-    state::RemoteState buffer_;
+    state::RemoteBuffer buffer_;
     WorldState state_;
 };
 
