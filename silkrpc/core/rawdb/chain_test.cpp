069df10736d1b0baff834b678156a806f452e963
canepat <16927169+canepat@users.noreply.github.com>
2022-03-14 12:12:17 +0100

    Update chain_test.cpp

--- silkrpc/core/rawdb/chain_test.cpp
+++ silkrpc/core/rawdb/chain_test.cpp
@@ -918,14 +918,14 @@ TEST_CASE("read_body") {
         CHECK_THROWS_AS(result.get(), std::runtime_error);
     }
 
-     SECTION("block found and matching") {
+    SECTION("block found and matching") {
         const auto block_hash{0x439816753229fc0736bf86a5048de4bc9fcdede8c91dadf88c828c76b2281dff_bytes32};
         const uint64_t block_number{4'000'000};
         EXPECT_CALL(db_reader, get(db::table::kBlockBodies, _)).WillOnce(InvokeWithoutArgs(
             []() -> asio::awaitable<KeyValue> { co_return KeyValue{silkworm::Bytes{}, kBody}; }
         ));
         EXPECT_CALL(db_reader, walk(db::table::kEthTx, _, _, _)).WillOnce(InvokeWithoutArgs(
-             []() -> asio::awaitable<void> { co_return; }
+            []() -> asio::awaitable<void> { co_return; }
         ));
         auto result = asio::co_spawn(pool, read_body(db_reader, block_hash, block_number), asio::use_future);
         const silkworm::BlockBody body = result.get();
