fb239e25908c7810cbb58339bc3b5445a3026bfa
Paolob69 <bruzzopa69@gmail.com>
2022-03-19 22:07:34 +0100

    read_block_by_transaction_hash throght cache

--- silkrpc/commands/debug_api.cpp
+++ silkrpc/commands/debug_api.cpp
@@ -287,15 +287,15 @@ asio::awaitable<void> DebugRpcApi::handle_debug_trace_transaction(const nlohmann
 
     try {
         ethdb::TransactionDatabase tx_database{*tx};
-        auto optional_transaction = co_await core::rawdb::read_transaction_by_hash(tx_database, transaction_hash);
-        if (!optional_transaction) {
+        auto optional_block_and_transaction = co_await core::read_transaction_by_hash(*context_.block_cache, tx_database, transaction_hash);
+        if (!optional_block_and_transaction) {
             std::ostringstream oss;
             oss << "transaction 0x" << transaction_hash << " not found";
             reply = make_json_error(request["id"], -32000, oss.str());
         } else {
-            auto block_with_hash = co_await core::read_block_by_transaction_hash(*context_.block_cache, tx_database, transaction_hash);
+            //auto block_with_hash = co_await core::read_block_by_transaction_hash(*context_.block_cache, tx_database, transaction_hash);
             trace::TraceExecutor executor{context_, tx_database, workers_, config};
-            const auto result = co_await executor.execute(block_with_hash.block, *optional_transaction);
+            const auto result = co_await executor.execute(optional_block_and_transaction->block_with_hash.block, optional_block_and_transaction->transaction);
 
             if (result.pre_check_error) {
                 reply = make_json_error(request["id"], -32000, result.pre_check_error.value());
