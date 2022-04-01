8479ba2a1d798ac927617becedb2e198fc766530
Enrique Jose  Avila Asapche <eavilaasapche@gmail.com>
2022-04-01 07:12:20 +0100

    Renaming tg API namespace as erigon (#258)
    Closes #101

--- silkrpc/common/constants.hpp
+++ silkrpc/common/constants.hpp
@@ -27,7 +27,7 @@ constexpr const char* kEngineApiNamespace{"engine"};
 constexpr const char* kEthApiNamespace{"eth"};
 constexpr const char* kNetApiNamespace{"net"};
 constexpr const char* kParityApiNamespace{"parity"};
-constexpr const char* kTgApiNamespace{"tg"};
+constexpr const char* kErigonApiNamespace{"erigon"};
 constexpr const char* kTraceApiNamespace{"trace"};
 constexpr const char* kWeb3ApiNamespace{"web3"};
 
