0731afda43c385933ab6ff31b4d31999fe27552a
Paolob69 <bruzzopa69@gmail.com>
2022-04-13 00:00:28 +0200

    add new method

--- silkrpc/commands/eth_api.hpp
+++ silkrpc/commands/eth_api.hpp
@@ -98,8 +98,10 @@ protected:
     asio::awaitable<void> handle_eth_submit_work(const nlohmann::json& request, nlohmann::json& reply);
     asio::awaitable<void> handle_eth_subscribe(const nlohmann::json& request, nlohmann::json& reply);
     asio::awaitable<void> handle_eth_unsubscribe(const nlohmann::json& request, nlohmann::json& reply);
+    asio::awaitable<void> handle_txpool_status(const nlohmann::json& request, nlohmann::json& reply);
     asio::awaitable<roaring::Roaring> get_topics_bitmap(core::rawdb::DatabaseReader& db_reader, FilterTopics& topics, uint64_t start, uint64_t end);
     asio::awaitable<roaring::Roaring> get_addresses_bitmap(core::rawdb::DatabaseReader& db_reader, FilterAddresses& addresses, uint64_t start, uint64_t end);
+
     std::vector<Log> filter_logs(std::vector<Log>& logs, const Filter& filter);
 
     Context& context_;
