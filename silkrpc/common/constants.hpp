c5bddc93dee9f8eb588f7eaccb4a00c7065d82e3
Paolob69 <bruzzopa69@gmail.com>
2022-04-13 00:18:58 +0200

    add txpool as default api spec

--- silkrpc/common/constants.hpp
+++ silkrpc/common/constants.hpp
@@ -39,7 +39,7 @@ constexpr const char* kEmptyChainData{""};
 constexpr const char* kDefaultEth1Local{"localhost:8545"};
 constexpr const char* kDefaultEth2Local{"localhost:8550"};
 constexpr const char* kDefaultTarget{"localhost:9090"};
-constexpr const char* kDefaultEth1ApiSpec{"debug,eth,net,parity,erigon,trace,web3"};
+constexpr const char* kDefaultEth1ApiSpec{"debug,eth,net,parity,erigon,trace,web3,txpool"};
 constexpr const char* kDefaultEth2ApiSpec{"engine"};
 constexpr const std::chrono::milliseconds kDefaultTimeout{10000};
 
