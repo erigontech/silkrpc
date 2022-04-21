b69e390799ce04f4145c6e6a71cc1deac5fd211b
lupin012 <bruzzopa69@gmail.com>
2022-04-21 20:08:50 +0200

    silkrpc/core/evm_executor.cpp

--- silkrpc/core/evm_executor.hpp
+++ silkrpc/core/evm_executor.hpp
@@ -56,10 +56,12 @@ public:
     EVMExecutor(const EVMExecutor&) = delete;
     EVMExecutor& operator=(const EVMExecutor&) = delete;
 
-    asio::awaitable<ExecutionResult> call(const silkworm::Block& block, const silkworm::Transaction& txn, std::shared_ptr<silkworm::EvmTracer> tracer = {});
+    asio::awaitable<ExecutionResult> call(const silkworm::Block& block, const silkworm::Transaction& txn, bool refund = true, std::shared_ptr<silkworm::EvmTracer> tracer = {});
 
 private:
     std::optional<std::string> pre_check(const VM& evm, const silkworm::Transaction& txn, const intx::uint256 base_fee_per_gas, const intx::uint128 g0);
+    uint64_t refund_gas(const VM& evm, const silkworm::Transaction& txn, uint64_t gas_left);
+
 
     const Context& context_;
     const core::rawdb::DatabaseReader& db_reader_;
