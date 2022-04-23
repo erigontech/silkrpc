cc0adfb38b95e216ad0e75c7cc1b2875685a3fdf
lupin012 <bruzzopa69@gmail.com>
2022-04-23 10:32:22 +0200

    add << for silkworm::Transaction

--- silkrpc/types/transaction.cpp
+++ silkrpc/types/transaction.cpp
@@ -65,4 +65,37 @@ std::ostream& operator<<(std::ostream& out, const Transaction& t) {
     return out;
 }
 
+std::ostream& operator<<(std::ostream& out, const silkworm::Transaction& t) {
+    out << " #access_list: " << t.access_list.size();
+    if (t.chain_id) {
+        out << " chain_id: " << silkworm::to_hex(silkworm::endian::to_big_compact(*t.chain_id));
+    } else {
+        out << " chain_id: null";
+    }
+    out << " data: " << silkworm::to_hex(t.data);
+    if (t.from) {
+        out << " from: " << silkworm::to_hex(*t.from);
+    } else {
+        out << " from: null";
+    }
+    out << " nonce: " << t.nonce;
+    out << " max_priority_fee_per_gas: " << silkworm::to_hex(silkworm::endian::to_big_compact(t.max_priority_fee_per_gas));
+    out << " max_fee_per_gas: " << silkworm::to_hex(silkworm::endian::to_big_compact(t.max_fee_per_gas));
+    out << " gas_limit: " << t.gas_limit;
+    out << " odd_y_parity: " << t.odd_y_parity;
+
+    out << " r: " << silkworm::to_hex(silkworm::endian::to_big_compact(t.r));
+    out << " s: " << silkworm::to_hex(silkworm::endian::to_big_compact(t.s));
+
+    if (t.to) {
+        out << " to: " << silkworm::to_hex(*t.to);
+    } else {
+        out << " to: null";
+    }
+    out << " type: 0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(t.type);
+    out << " value: " << silkworm::to_hex(silkworm::endian::to_big_compact(t.value));
+
+    return out;
+}
+
 } // namespace silkrpc
