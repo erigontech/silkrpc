400cd571a96484a82bd65244901d5799570e3ca1
lupin012 <bruzzopa69@gmail.com>
2022-04-23 10:31:37 +0200

    add using EvmTracer

--- silkrpc/core/evm_executor.hpp
+++ silkrpc/core/evm_executor.hpp
@@ -44,6 +44,8 @@ struct ExecutionResult {
     std::optional<std::string> pre_check_error{std::nullopt};
 };
 
+using EvmTracerCall = std::shared_ptr<silkworm::EvmTracer>;
+
 template<typename WorldState = silkworm::IntraBlockState, typename VM = silkworm::EVM>
 class EVMExecutor {
 public:
@@ -56,8 +58,7 @@ public:
     EVMExecutor(const EVMExecutor&) = delete;
     EVMExecutor& operator=(const EVMExecutor&) = delete;
 
-    asio::awaitable<ExecutionResult> call(const silkworm::Block& block, const silkworm::Transaction& txn, bool refund = true,
-                                          bool gas_bailout = false, std::shared_ptr<silkworm::EvmTracer> tracer = {});
+    asio::awaitable<ExecutionResult> call(const silkworm::Block& block, const silkworm::Transaction& txn, bool refund = true, bool gas_bailout = false, EvmTracerCall tracer = {});
 
 private:
     std::optional<std::string> pre_check(const VM& evm, const silkworm::Transaction& txn, const intx::uint256 base_fee_per_gas, const intx::uint128 g0);
