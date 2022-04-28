ecb218ef16fdc62035696904e78f573e8622d0ac
Enrique Jose  Avila Asapche <eavilaasapche@gmail.com>
2022-04-28 20:01:30 +0100

    Added eth API namespace for Engine end-point (#271)

--- silkrpc/common/constants.hpp
+++ silkrpc/common/constants.hpp
@@ -40,7 +40,7 @@ constexpr const char* kDefaultHttpPort{"localhost:8545"};
 constexpr const char* kDefaultEnginePort{"localhost:8550"};
 constexpr const char* kDefaultTarget{"localhost:9090"};
 constexpr const char* kDefaultEth1ApiSpec{"debug,eth,net,parity,erigon,trace,web3,txpool"};
-constexpr const char* kDefaultEth2ApiSpec{"engine"};
+constexpr const char* kDefaultEth2ApiSpec{"engine,eth"};
 constexpr const std::chrono::milliseconds kDefaultTimeout{10000};
 
 constexpr const std::size_t kHttpIncomingBufferSize{8192};
