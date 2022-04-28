193b5219d81178f7c522b5f605b731654a909d1a
lupin012 <58134934+lupin012@users.noreply.github.com>
2022-04-28 18:51:54 +0200

    Add gas refund to EVM executor (#268)

--- silkrpc/core/evm_executor.hpp
+++ silkrpc/core/evm_executor.hpp
@@ -44,6 +44,8 @@ struct ExecutionResult {
     std::optional<std::string> pre_check_error{std::nullopt};
 };
 
+using EvmTracerCall = std::shared_ptr<silkworm::EvmTracer>;
+
 template<typename WorldState = silkworm::IntraBlockState, typename VM = silkworm::EVM>
 class EVMExecutor {
 public:
@@ -56,10 +58,11 @@ public:
     EVMExecutor(const EVMExecutor&) = delete;
     EVMExecutor& operator=(const EVMExecutor&) = delete;
 
-    asio::awaitable<ExecutionResult> call(const silkworm::Block& block, const silkworm::Transaction& txn, std::shared_ptr<silkworm::EvmTracer> tracer = {});
+    asio::awaitable<ExecutionResult> call(const silkworm::Block& block, const silkworm::Transaction& txn, bool refund = true, bool gas_bailout = false, EvmTracerCall tracer = {});
 
 private:
     std::optional<std::string> pre_check(const VM& evm, const silkworm::Transaction& txn, const intx::uint256 base_fee_per_gas, const intx::uint128 g0);
+    uint64_t refund_gas(const VM& evm, const silkworm::Transaction& txn, uint64_t gas_left);
 
     const Context& context_;
     const core::rawdb::DatabaseReader& db_reader_;
