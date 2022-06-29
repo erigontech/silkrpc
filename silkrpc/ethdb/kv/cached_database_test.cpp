1bc7433d8040b0f6bc2798322899efd15a60cdff
canepat <16927169+canepat@users.noreply.github.com>
2022-06-29 12:44:31 +0200

    Add unit tests

--- silkrpc/ethdb/kv/cached_database_test.cpp
+++ silkrpc/ethdb/kv/cached_database_test.cpp
@@ -16,11 +16,16 @@
 
 #include "cached_database.hpp"
 
+#include <asio/co_spawn.hpp>
 #include <asio/thread_pool.hpp>
+#include <asio/use_future.hpp>
 #include <catch2/catch.hpp>
 #include <gmock/gmock.h>
 
 #include <silkrpc/ethdb/kv/state_cache.hpp>
+#include <silkrpc/ethdb/tables.hpp>
+#include <silkrpc/test/dummy_transaction.hpp>
+#include <silkrpc/test/mock_cursor.hpp>
 #include <silkrpc/test/mock_transaction.hpp>
 #include <silkrpc/types/block.hpp>
 
@@ -30,14 +35,47 @@ using Catch::Matchers::Message;
 using testing::_;
 using testing::InvokeWithoutArgs;
 
-TEST_CASE("CachedDatabase", "[silkrpc][ethdb][kv][cached_reader]") {
+TEST_CASE("CachedDatabase::CachedDatabase", "[silkrpc][ethdb][kv][cached_reader]") {
     BlockNumberOrHash block_id{0};
     test::MockTransaction txn;
     kv::CoherentStateCache cache;
-    CachedDatabase database{block_id, txn, cache};
-    asio::thread_pool pool{1};
+    CHECK_NOTHROW(CachedDatabase{block_id, txn, cache});
+}
 
-    // TODO(canepat) complete
+/*TEST_CASE("CachedDatabase::get_one", "[silkrpc][ethdb][kv][cached_reader]") {
+    asio::thread_pool pool{1};
+    std::shared_ptr<test::MockCursor> mock_cursor = std::make_shared<test::MockCursor>();
+    test::DummyTransaction txn{0, mock_cursor};
+    kv::CoherentStateCache cache;
+    
+    SECTION("empty key from PlainState in latest block") {
+        BlockNumberOrHash block_id{0};
+        CachedDatabase cached_db{block_id, txn, cache};
+        EXPECT_CALL(*mock_cursor, seek_exact(_)).WillOnce(InvokeWithoutArgs([]() -> asio::awaitable<KeyValue> {
+            co_return KeyValue{silkworm::Bytes{}, silkworm::Bytes{}};
+        }));
+        auto result = asio::co_spawn(pool, cached_db.get_one(db::table::kPlainState, silkworm::Bytes{}), asio::use_future);
+        const auto value = result.get();
+        CHECK(value.empty());
+    }
+}*/
+
+TEST_CASE("CachedDatabase::get", "[silkrpc][ethdb][kv][cached_reader]") {
+    asio::thread_pool pool{1};
+    std::shared_ptr<test::MockCursor> mock_cursor = std::make_shared<test::MockCursor>();
+    test::DummyTransaction txn{0, mock_cursor};
+    kv::CoherentStateCache cache;
+    
+    SECTION("empty key from PlainState in latest block") {
+        BlockNumberOrHash block_id{0};
+        CachedDatabase cached_db{block_id, txn, cache};
+        EXPECT_CALL(*mock_cursor, seek(_)).WillOnce(InvokeWithoutArgs([]() -> asio::awaitable<KeyValue> {
+            co_return KeyValue{silkworm::Bytes{}, silkworm::Bytes{}};
+        }));
+        auto result = asio::co_spawn(pool, cached_db.get(db::table::kPlainState, silkworm::Bytes{}), asio::use_future);
+        const auto kv = result.get();
+        CHECK(kv.value.empty());
+    }
 }
 
 }  // namespace silkrpc::ethdb::kv
