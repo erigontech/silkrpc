1cadfec04f455282b33f30ab694b63e854ceccc7
Sixtysixter <giacomo66.guarguaglini@gmail.com>
2022-07-25 22:07:39 +0200

    Add trace_replayBlockTransactions API implementation (#325)

--- silkrpc/commands/trace_api.cpp
+++ silkrpc/commands/trace_api.cpp
@@ -44,21 +44,8 @@ asio::awaitable<void> TraceRpcApi::handle_trace_call(const nlohmann::json& reque
         co_return;
     }
     const auto call = params[0].get<Call>();
-    const auto trace_type = params[1].get<std::vector<std::string>>();
+    const auto config = params[1].get<trace::TraceConfig>();
     const auto block_number_or_hash = params[2].get<BlockNumberOrHash>();
-
-    trace::TraceConfig config;
-    for (auto entry : trace_type) {
-        if (entry == "trace") {
-            config.trace = true;
-        }
-        if (entry == "vmTrace") {
-            config.vm_trace = true;
-        }
-        if (entry == "stateDiff") {
-            config.state_diff = true;
-        }
-    }
 
     SILKRPC_INFO << "call: " << call << " block_number_or_hash: " << block_number_or_hash << " config: " << config << "\n";
 
@@ -131,12 +118,28 @@ asio::awaitable<void> TraceRpcApi::handle_trace_raw_transaction(const nlohmann::
 
 // https://eth.wiki/json-rpc/API#trace_replayblocktransactions
 asio::awaitable<void> TraceRpcApi::handle_trace_replay_block_transactions(const nlohmann::json& request, nlohmann::json& reply) {
+    auto params = request["params"];
+    if (params.size() < 2) {
+        auto error_msg = "invalid trace_replayBlockTransactions params: " + params.dump();
+        SILKRPC_ERROR << error_msg << "\n";
+        reply = make_json_error(request["id"], 100, error_msg);
+        co_return;
+    }
+    const auto block_number_or_hash = params[0].get<BlockNumberOrHash>();
+    const auto config = params[1].get<trace::TraceConfig>();
+
+    SILKRPC_INFO << " block_number_or_hash: " << block_number_or_hash << " config: " << config << "\n";
+
     auto tx = co_await database_->begin();
 
     try {
         ethdb::TransactionDatabase tx_database{*tx};
 
-        reply = make_json_error(request["id"], 500, "not yet implemented");
+        const auto block_with_hash = co_await core::read_block_by_number_or_hash(*context_.block_cache(), tx_database, block_number_or_hash);
+
+        trace::TraceCallExecutor executor{*context_.io_context(), tx_database, workers_, config};
+        auto result = co_await executor.execute(block_with_hash.block);
+        reply = make_json_content(request["id"], result);
     } catch (const std::exception& e) {
         SILKRPC_ERROR << "exception: " << e.what() << " processing request: " << request.dump() << "\n";
         reply = make_json_error(request["id"], 100, e.what());
@@ -159,20 +162,7 @@ asio::awaitable<void> TraceRpcApi::handle_trace_replay_transaction(const nlohman
         co_return;
     }
     const auto transaction_hash = params[0].get<evmc::bytes32>();
-    const auto trace_type = params[1].get<std::vector<std::string>>();
-
-    trace::TraceConfig config;
-    for (const auto entry : trace_type) {
-        if (entry == "trace") {
-            config.trace = true;
-        }
-        if (entry == "vmTrace") {
-            config.vm_trace = true;
-        }
-        if (entry == "stateDiff") {
-            config.state_diff = true;
-        }
-    }
+    const auto config = params[1].get<trace::TraceConfig>();
 
     SILKRPC_INFO << "transaction_hash: " << transaction_hash << " config: " << config << "\n";
 
