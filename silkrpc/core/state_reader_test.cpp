b316c0b9d7f7b2e99ae1c97145ca0ebaa44e1be4
canepat <16927169+canepat@users.noreply.github.com>
2022-08-02 21:42:44 +0200

    Add unit tests

--- silkrpc/core/state_reader_test.cpp
+++ silkrpc/core/state_reader_test.cpp
@@ -16,11 +16,74 @@
 
 #include "state_reader.hpp"
 
+#include <silkrpc/config.hpp>
+#include <asio/awaitable.hpp>
 #include <catch2/catch.hpp>
+#include <evmc/evmc.hpp>
+
+#include <silkrpc/common/util.hpp>
+#include <silkrpc/core/blocks.hpp>
+#include <silkrpc/ethdb/tables.hpp>
+#include <silkrpc/test/context_test_base.hpp>
+#include <silkrpc/test/mock_database_reader.hpp>
 
 namespace silkrpc {
 
-using Catch::Matchers::Message;
+using evmc::literals::operator""_bytes32;
+using testing::InvokeWithoutArgs;
+using testing::_;
+
+static const evmc::address kZeroAddress{};
+static const silkworm::Bytes kEncodedAccount{*silkworm::from_hex(
+    "0f01020203e8010520f1885eda54b7a053318cd41e2093220dab15d65381b1157a3633a83bfd5c9239")};
+
+struct StateReaderTest : public test::ContextTestBase {
+    test::MockDatabaseReader database_reader_;
+    StateReader state_reader_{database_reader_};
+};
+
+TEST_CASE_METHOD(StateReaderTest, "StateReader::read_account") {
+    SILKRPC_LOG_VERBOSITY(LogLevel::Trace);
+    SILKRPC_DEBUG << "kEncodedAccount: " << silkworm::to_hex(kEncodedAccount) << "\n";
+
+    SECTION("no account for history empty and current state empty") {
+        // Set the call expectations:
+        // 1. DatabaseReader::get call on kAccountHistory returns empty key-value
+        EXPECT_CALL(database_reader_, get(db::table::kAccountHistory, _)).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<KeyValue> { co_return KeyValue{}; }
+        ));
+        // 2. DatabaseReader::get_one call on kPlainState returns empty value
+        EXPECT_CALL(database_reader_, get_one(db::table::kPlainState, full_view(kZeroAddress))).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<silkworm::Bytes> { co_return silkworm::Bytes{}; }
+        ));
+
+        // Execute the test: calling read_account should return no account
+        std::optional<silkworm::Account> account;
+        CHECK_NOTHROW(account = spawn_and_wait(state_reader_.read_account(kZeroAddress, core::kEarliestBlockNumber)));
+        CHECK(!account.has_value());
+    }
+
+    SECTION("account found in current state") {
+        // Set the call expectations:
+        // 1. DatabaseReader::get call on kAccountHistory returns empty key-value
+        EXPECT_CALL(database_reader_, get(db::table::kAccountHistory, _)).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<KeyValue> { co_return KeyValue{}; }
+        ));
+        // 2. DatabaseReader::get_one call on kPlainState returns account data
+        EXPECT_CALL(database_reader_, get_one(db::table::kPlainState, full_view(kZeroAddress))).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<silkworm::Bytes> { co_return kEncodedAccount; }
+        ));
+
+        // Execute the test: calling read_account should return expected account
+        std::optional<silkworm::Account> account;
+        CHECK_NOTHROW(account = spawn_and_wait(state_reader_.read_account(kZeroAddress, core::kEarliestBlockNumber)));
+        CHECK(account.has_value());
+        CHECK(account->nonce == 2);
+        CHECK(account->balance == 1000);
+        CHECK(account->code_hash == 0xf1885eda54b7a053318cd41e2093220dab15d65381b1157a3633a83bfd5c9239_bytes32);
+        CHECK(account->incarnation == 5);
+    }
+}
 
 } // namespace silkrpc
 
