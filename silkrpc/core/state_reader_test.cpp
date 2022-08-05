83042e5a1fedd8fde8721d5779b9a369ebdcdd39
canepat <16927169+canepat@users.noreply.github.com>
2022-08-05 16:48:14 +0200

    Add unit test

--- silkrpc/core/state_reader_test.cpp
+++ silkrpc/core/state_reader_test.cpp
@@ -40,6 +40,8 @@ static const silkworm::Bytes kEncodedAccountHistory{*silkworm::from_hex(
     "0100000000000000000000003a300000020000006e001b006f000f001800000050000000e042e442f5420e4320"
     "432d433c4343435e436343e550e750eb50a160a4604f6175c504c6c7cedaceeccedbd0f3d0f4d001d104d105d1"
     "09d18613b113061e801f861fd11fda1fe6323833ac3943651f67226c406d36706f70")};
+static const silkworm::Bytes kEncodedAccountWithoutCodeHash{*silkworm::from_hex(
+    "07023039080de0b6b3a76400000105")};
 
 static const evmc::bytes32 kLocationHash{};
 static const silkworm::Bytes kStorageLocation{*silkworm::from_hex(
@@ -121,6 +123,33 @@ TEST_CASE_METHOD(StateReaderTest, "StateReader::read_account") {
             CHECK(account->nonce == 2);
             CHECK(account->balance == 1000);
             CHECK(account->code_hash == 0xf1885eda54b7a053318cd41e2093220dab15d65381b1157a3633a83bfd5c9239_bytes32);
+            CHECK(account->incarnation == 5);
+        }
+    }
+
+    SECTION("account w/o code hash found current state") {
+        // Set the call expectations:
+        // 1. DatabaseReader::get call on kAccountHistory returns empty key-value
+        EXPECT_CALL(database_reader_, get(db::table::kAccountHistory, _)).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<KeyValue> { co_return KeyValue{}; }
+        ));
+        // 2. DatabaseReader::get_one call on kPlainState returns account data
+        EXPECT_CALL(database_reader_, get_one(db::table::kPlainState, full_view(kZeroAddress))).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<silkworm::Bytes> { co_return kEncodedAccountWithoutCodeHash; }
+        ));
+        // 3. DatabaseReader::get_one call on kPlainContractCode returns account code hash
+        EXPECT_CALL(database_reader_, get_one(db::table::kPlainContractCode, _)).WillOnce(InvokeWithoutArgs(
+            []() -> asio::awaitable<silkworm::Bytes> { co_return silkworm::Bytes{kCodeHash.bytes, silkworm::kHashLength}; }
+        ));
+
+        // Execute the test: calling read_account should return the expected account
+        std::optional<silkworm::Account> account;
+        CHECK_NOTHROW(account = spawn_and_wait(state_reader_.read_account(kZeroAddress, core::kEarliestBlockNumber)));
+        CHECK(account);
+        if (account) {
+            CHECK(account->nonce == 12345);
+            CHECK(account->balance == silkworm::kEther);
+            CHECK(account->code_hash == kCodeHash);
             CHECK(account->incarnation == 5);
         }
     }
