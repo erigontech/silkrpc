9f9ff7d39ee2f3c410689133d484feabc0d4eaa5
giacomo66.guarguaglini <giacomo66.guarguaglini@gmail.com>
2022-08-10 15:46:33 +0200

    Implemented compute_reward

--- silkrpc/consensus/ethash.cpp
+++ silkrpc/consensus/ethash.cpp
@@ -16,15 +16,43 @@
 
 #include "ethash.hpp"
 
+#include <silkworm/core/silkworm/chain/protocol_param.hpp>
+#include <silkrpc/json/types.hpp>
+
 namespace silkrpc::ethash {
 
 BlockReward compute_reward(const ChainConfig& config, const silkworm::Block& block) {
-    return BlockReward{};
+    auto chain_config = silkworm::ChainConfig::from_json(config.config).value();
+    auto revision = chain_config.revision(block.header.number);
+    BlockReward block_reward;
+    block_reward.miner_reward = silkworm::param::kBlockRewardFrontier;
+    if (revision > evmc_revision::EVMC_BYZANTIUM) {
+        block_reward.miner_reward = silkworm::param::kBlockRewardByzantium;
+    }
+    if (revision > evmc_revision::EVMC_CONSTANTINOPLE) {
+        block_reward.miner_reward = silkworm::param::kBlockRewardConstantinople;
+    }
+
+    // Accumulate the rewards for the miner and any included uncles
+    intx::uint256 ommer_reward;
+    auto header_number = block.header.number;
+    for (auto uncle : block.ommers) {
+        ommer_reward = uncle.number + 8;
+        ommer_reward -= header_number;
+        ommer_reward *= block_reward.miner_reward;
+        ommer_reward /= 8;
+        block_reward.ommer_rewards.push_back(ommer_reward);
+
+        ommer_reward = block_reward.miner_reward / 32;
+
+        block_reward.miner_reward += ommer_reward;
+    }
+
+    return block_reward;
 }
 
 std::ostream& operator<<(std::ostream& out, const BlockReward& reward) {
-    out << "miner_reward: " << intx::to_string(reward.miner_reward) << " "
-        << "ommer_rewards: [";
+    out << "miner_reward: " << intx::to_string(reward.miner_reward) << " ommer_rewards: [";
     for (auto i{0}; i < reward.ommer_rewards.size(); i++) {
         out << intx::to_string(reward.ommer_rewards[i]);
         if (i != reward.ommer_rewards.size() - 1) {
