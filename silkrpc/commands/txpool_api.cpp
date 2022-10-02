206c84c6990f85d8a5dfc474aff65909da2cd627
lupin012 <SimonSmith2021@libero.it>
2022-10-02 13:36:38 +0200

    change rlp api

--- silkrpc/commands/txpool_api.cpp
+++ silkrpc/commands/txpool_api.cpp
@@ -53,7 +53,7 @@ boost::asio::awaitable<void> TxPoolRpcApi::handle_txpool_content(const nlohmann:
             silkworm::ByteView from{txpool_transactions[i].rlp};
             std::string sender = silkworm::to_hex(txpool_transactions[i].sender, true);
             Transaction txn{};
-            const auto result = silkworm::rlp::decode(from, dynamic_cast<silkworm::Transaction&>(txn));
+            const auto result = silkworm::rlp::decode_transaction(from, dynamic_cast<silkworm::Transaction&>(txn), silkworm::rlp::Eip2718Wrapping::kBoth);
             if (result != silkworm::DecodingResult::kOk) {
                 SILKRPC_ERROR << "handle_txpool_content  rlp::decode failed sender: " << sender << "\n";
                 error = true;
