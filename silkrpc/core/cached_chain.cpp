6dc9f4f4a02d89f70664bb2bf32d9f2343e32901
lupin012 <SimonSmith2021@libero.it>
2022-10-11 19:23:40 +0200

    modify comment

--- silkrpc/core/cached_chain.cpp
+++ silkrpc/core/cached_chain.cpp
@@ -28,7 +28,9 @@ boost::asio::awaitable<silkworm::BlockWithHash> read_block_by_number(BlockCache&
         co_return cached_block.value();
     }
     auto block_with_hash = co_await rawdb::read_block(reader, block_hash, block_number);
-    if (block_with_hash.block.transactions.size() != 0) { // don't save empty blocks to cache, if block become non-canonical, we remove it's transactions,but block can canonical in future
+    if (block_with_hash.block.transactions.size() != 0) { 
+       // don't save empty (without txs) blocks to cache, if block become non-canonical (not in main chain), we remove it's transactions, 
+       // but block can in the future become canonical(inserted in main chain) with its transactions
        cache.insert(block_hash, block_with_hash);
     }
     co_return block_with_hash;
@@ -40,7 +42,9 @@ boost::asio::awaitable<silkworm::BlockWithHash> read_block_by_hash(BlockCache& c
         co_return cached_block.value();
     }
     auto block_with_hash = co_await rawdb::read_block_by_hash(reader, block_hash);
-    if (block_with_hash.block.transactions.size() != 0) { // don't save empty blocks to cache, if block become non-canonical, we remove it's transactions,but block become canonical in future
+    if (block_with_hash.block.transactions.size() != 0) { 
+       // don't save empty (without txs) blocks to cache, if block become non-canonical (not in main chain), we remove it's transactions, 
+       // but block can in the future become canonical(inserted in main chain) with its transactions
        cache.insert(block_hash, block_with_hash);
     }
     co_return block_with_hash;
