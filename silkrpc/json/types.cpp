97352974212868886130c78de2716d2a6f01dafe
canepat <16927169+canepat@users.noreply.github.com>
2022-10-14 14:37:49 +0200

    Fixes from Clang tidy in ethbackend and json folders

--- silkrpc/json/types.cpp
+++ silkrpc/json/types.cpp
@@ -544,14 +544,14 @@ void from_json(const nlohmann::json& json, ExecutionPayload& execution_payload) 
     };
 }
 
-void to_json(nlohmann::json& json, const ForkchoiceState& forkchoice_state) {
+void to_json(nlohmann::json& json, const ForkChoiceState& forkchoice_state) {
     json["headBlockHash"] = forkchoice_state.head_block_hash;
     json["safeBlockHash"] = forkchoice_state.safe_block_hash;
     json["finalizedBlockHash"] = forkchoice_state.finalized_block_hash;
 }
 
-void from_json(const nlohmann::json& json, ForkchoiceState& forkchoice_state) {
-    forkchoice_state = ForkchoiceState{
+void from_json(const nlohmann::json& json, ForkChoiceState& forkchoice_state) {
+    forkchoice_state = ForkChoiceState{
         .head_block_hash = json.at("headBlockHash").get<evmc::bytes32>(),
         .safe_block_hash = json.at("safeBlockHash").get<evmc::bytes32>(),
         .finalized_block_hash = json.at("finalizedBlockHash").get<evmc::bytes32>()
@@ -572,7 +572,7 @@ void from_json(const nlohmann::json& json, PayloadAttributes& payload_attributes
     };
 }
 
-void to_json(nlohmann::json& json, const ForkchoiceUpdatedReply& forkchoice_updated_reply) {
+void to_json(nlohmann::json& json, const ForkChoiceUpdatedReply& forkchoice_updated_reply) {
     nlohmann::json json_payload_status = forkchoice_updated_reply.payload_status;
     json["payloadStatus"] = json_payload_status;
     if (forkchoice_updated_reply.payload_id != std::nullopt) {
