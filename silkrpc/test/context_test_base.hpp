97b4262580392413d6ab0f9b42015f80a89d50d3
canepat <16927169+canepat@users.noreply.github.com>
2022-10-15 08:37:13 +0200

    Fixes from Clang-tidy in silkrpc folder (#354)

--- silkrpc/test/context_test_base.hpp
+++ silkrpc/test/context_test_base.hpp
@@ -44,7 +44,7 @@ class ContextTestBase {
         return spawn(std::forward<AwaitableOrFunction>(awaitable)).get();
     }
 
-    void sleep_for(std::chrono::milliseconds sleep_time_ms) {
+    static void sleep_for(std::chrono::milliseconds sleep_time_ms) {
         std::this_thread::sleep_for(sleep_time_ms);
     }
 
