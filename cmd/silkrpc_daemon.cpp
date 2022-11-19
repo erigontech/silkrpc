45551e3cfaefd1cbfb5541c3f9f91fc0b5544dda
lupin012 <SimonSmith2021@libero.it>
2022-11-19 17:11:05 +0100

    mgt auth port

--- cmd/silkrpc_daemon.cpp
+++ cmd/silkrpc_daemon.cpp
@@ -34,6 +34,7 @@
 ABSL_FLAG(std::string, chaindata, silkrpc::kEmptyChainData, "chain data path as string");
 ABSL_FLAG(std::string, http_port, silkrpc::kDefaultHttpPort, "Ethereum JSON RPC API local end-point as string <address>:<port>");
 ABSL_FLAG(std::string, engine_port, silkrpc::kDefaultEnginePort, "Engine JSON RPC API local end-point as string <address>:<port>");
+ABSL_FLAG(std::string, auth_engine_port, silkrpc::kDefaultAuthEnginePort, "Engine JSON RPC API local end-point as string <address>:<port>");
 ABSL_FLAG(std::string, target, silkrpc::kDefaultTarget, "Erigon Core gRPC service location as string <address>:<port>");
 ABSL_FLAG(std::string, api_spec, silkrpc::kDefaultEth1ApiSpec, "JSON RPC API namespaces as comma-separated list of strings");
 ABSL_FLAG(uint32_t, num_contexts, std::thread::hardware_concurrency() / 3, "number of running I/O contexts as 32-bit integer");
@@ -41,6 +42,7 @@ ABSL_FLAG(uint32_t, num_workers, 16, "number of worker threads as 32-bit integer
 ABSL_FLAG(uint32_t, timeout, silkrpc::kDefaultTimeout.count(), "gRPC call timeout as 32-bit integer");
 ABSL_FLAG(silkrpc::LogLevel, log_verbosity, silkrpc::LogLevel::Critical, "logging verbosity level");
 ABSL_FLAG(silkrpc::WaitMode, wait_mode, silkrpc::WaitMode::blocking, "scheduler wait mode");
+ABSL_FLAG(std::string, jwt_secret, silkrpc::kDefaultJwtSecret, "Token to ensure safe connection between CL and EL");
 
 //! Assemble the application version using the Cable build information
 std::string get_version_from_build_info() {
@@ -95,12 +97,14 @@ silkrpc::DaemonSettings parse_args(int argc, char* argv[]) {
         absl::GetFlag(FLAGS_chaindata),
         absl::GetFlag(FLAGS_http_port),
         absl::GetFlag(FLAGS_engine_port),
+        absl::GetFlag(FLAGS_auth_engine_port),
         absl::GetFlag(FLAGS_api_spec),
         absl::GetFlag(FLAGS_target),
         absl::GetFlag(FLAGS_num_contexts),
         absl::GetFlag(FLAGS_num_workers),
         absl::GetFlag(FLAGS_log_verbosity),
-        absl::GetFlag(FLAGS_wait_mode)
+        absl::GetFlag(FLAGS_wait_mode),
+        absl::GetFlag(FLAGS_jwt_secret)
     };
 
     return rpc_daemon_settings;
