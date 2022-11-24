a55c6f8bbe6c9ed6ebdf2ae98fb4474d81bca3e2
lupin012 <SimonSmith2021@libero.it>
2022-11-24 07:49:38 +0100

    change constant name for jwt file

--- silkrpc/common/constants.hpp
+++ silkrpc/common/constants.hpp
@@ -34,7 +34,7 @@ constexpr const char* kWeb3ApiNamespace{"web3"};
 
 constexpr const char* kAddressPortSeparator{":"};
 constexpr const char* kApiSpecSeparator{","};
-constexpr const char* kDefaultJwtPath{"jwt.hex"};
+constexpr const char* kDefaultJwtFilename{"jwt.hex"};
 
 constexpr const char* kEmptyChainData{""};
 constexpr const char* kDefaultHttpPort{"localhost:8545"};
@@ -43,7 +43,6 @@ constexpr const char* kDefaultAuthEnginePort{"localhost:8551"};
 constexpr const char* kDefaultTarget{"localhost:9090"};
 constexpr const char* kDefaultEth1ApiSpec{"debug,eth,net,parity,erigon,trace,web3,txpool"};
 constexpr const char* kDefaultEth2ApiSpec{"engine,eth"};
-constexpr const char* kDefaultJwtSecret{""};
 constexpr const std::chrono::milliseconds kDefaultTimeout{10000};
 
 constexpr const std::size_t kHttpIncomingBufferSize{8192};
