60bd8018e03f7455b8a739405b9a5e31d80f2bb6
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-01-08 15:50:15 +0100

    add node_info type

---
+++ silkrpc/types/node_info.hpp
@@ -0,0 +1,40 @@
+/*
+   Copyright 2020 The Silkrpc Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef SILKRPC_TYPES_NODE_INFO_HPP_
+#define SILKRPC_TYPES_NODE_INFO_HPP_
+
+#include <vector>
+#include <optional>
+#include <string>
+
+#include <evmc/evmc.hpp>
+#include <intx/intx.hpp>
+#include <silkworm/common/base.hpp>
+#include <silkworm/types/bloom.hpp>
+
+namespace silkrpc {
+
+struct NodeInfo {
+    std::string id;
+    std::string name;
+    std::string enode;
+    std::string enr;
+};
+
+} // namespace silkrpc
+
+#endif  // SILKRPC_TYPES_NODE_INFO_HPP_
