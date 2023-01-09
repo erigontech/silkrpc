9cfe1e068eb947a093fbf3c4a2b3bf81466289a5
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-01-10 00:17:58 +0100

    process all NodeInfo fields

--- silkrpc/types/node_info.hpp
+++ silkrpc/types/node_info.hpp
@@ -17,22 +17,63 @@
 #ifndef SILKRPC_TYPES_NODE_INFO_HPP_
 #define SILKRPC_TYPES_NODE_INFO_HPP_
 
-#include <vector>
 #include <optional>
 #include <string>
 
 #include <evmc/evmc.hpp>
-#include <intx/intx.hpp>
-#include <silkworm/common/base.hpp>
-#include <silkworm/types/bloom.hpp>
 
 namespace silkrpc {
 
+struct NodeInfoClique {
+    uint64_t period;
+    uint64_t epoch;
+};
+
+struct NodeInfoConfig {
+    std::string chain_name;
+    uint64_t chain_id;
+    std::string consensus;
+    uint64_t homestead_block;
+    bool  dao_fork_support;
+    uint64_t eip150_block;
+    evmc::bytes32 eip150_hash;
+    uint64_t eip155_block;
+    uint64_t byzantium_block;
+    uint64_t constantinople_block;
+    uint64_t petersburg_block;
+    uint64_t istanbul_block;
+    uint64_t berlin_block;
+    uint64_t london_block;
+    uint64_t terminal_total_difficulty;
+    bool  terminal_total_difficulty_passed;
+    std::optional<NodeInfoClique> clique;
+};
+
+struct NodeInfoPorts{
+    uint64_t discovery;
+    uint64_t listener;
+};
+
+struct NodeInfoEthereum {
+    uint64_t network;
+    uint64_t difficulty;
+    evmc::bytes32 genesis_hash;
+    NodeInfoConfig config;
+    evmc::bytes32 head;
+};
+
+struct NodeInfoProtocols {
+    NodeInfoEthereum eth;
+};
+
 struct NodeInfo {
-    std::string id;
-    std::string name;
-    std::string enode;
-    std::string enr;
+    std::string       id;
+    std::string       name;
+    std::string       enode;
+    std::string       enr;
+    std::string       listenerAddr;
+    NodeInfoProtocols protocols;
+    NodeInfoPorts     ports;
 };
 
 } // namespace silkrpc
