47e5256fed7664cecc2fb6495429196fdc0285b8
canepat <16927169+canepat@users.noreply.github.com>
2023-01-12 01:29:16 +0100

    Update types.cpp

--- silkrpc/json/types.cpp
+++ silkrpc/json/types.cpp
@@ -237,8 +237,7 @@ void to_json(nlohmann::json& json, const NodeInfo& node_info) {
     json["name"] = node_info.name;
     json["enode"] = node_info.enode;
     json["enr"] = node_info.enr;
-    json["listenAddr"] = node_info.listenerAddr;
-    json["listenAddr"] = node_info.listenerAddr;
+    json["listenAddr"] = node_info.listener_addr;
     json["ports"] = node_info.ports;
     json["ip"] = node_info.enode;
     json["protocols"] = nlohmann::json::parse(node_info.protocols, nullptr, /* allow_exceptions = */ false);
